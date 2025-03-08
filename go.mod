a06c1e3d5d5e7857c29c4185f6ae4b09a7d0c1a0
FanFusheng <ffs@ppno.net>
2025-03-08 13:11:09 +0800

    refactor(config): 重构配置文件加载逻辑
    - 替换原有的 yaml 文件读取和解析逻辑，使用 Kratos 框架的配置管理器
    - 更新配置文件路径和名称，从 sms-bridge 改为 message
    - 修改配置文件内容，更新 BasicAuth 认证配置
    - 更新 go.mod 和 go.sum 文件，添加新的依赖项

--- go.mod
+++ go.mod
@@ -12,7 +12,6 @@ require (
 	google.golang.org/genproto/googleapis/api v0.0.0-20250303144028-a0af3efb3deb
 	google.golang.org/grpc v1.71.0
 	google.golang.org/protobuf v1.36.5
-	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -40,6 +39,8 @@ require (
 )
 
 require (
+	dario.cat/mergo v1.0.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-kratos/aegis v0.2.0 // indirect
 	github.com/go-playground/form/v4 v4.2.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
@@ -51,4 +52,5 @@ require (
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250227231956-55c901821b1e // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -6,10 +6,10 @@
 package config
 
 import (
-	"fmt"
-	"os"
+	"github.com/go-kratos/kratos/v2/config"
+	"github.com/go-kratos/kratos/v2/config/file"
 
-	"gopkg.in/yaml.v3"
+	kit_kratos_config "github.com/fsyyft-go/kit/kratos/config"
 )
 
 // LoadConfig 从指定路径加载配置文件并解析为 Config 结构体。
@@ -21,16 +21,23 @@ import (
 //   - *Config：解析后的配置对象指针。
 //   - error：加载或解析过程中可能发生的错误。
 func LoadConfig(path string) (*Config, error) {
-	// 从 yaml 文件中读取配置。
-	data, err := os.ReadFile(path)
-	if nil != err {
-		return nil, fmt.Errorf("读取配置文件失败：%v", err)
+	// 创建配置管理器实例。
+	c := config.New(
+		// 设置配置源为文件源，指定配置文件路径。
+		config.WithSource(
+			file.NewSource(path),
+		),
+		// 设置自定义解码器，支持特殊格式处理（如 base64 解码）。
+		config.WithDecoder(kit_kratos_config.NewDecoder().Decode),
+	)
+	// 加载配置，如果出错则触发 panic。
+	if err := c.Load(); err != nil {
+		return nil, err
 	}
 
-	// 解析 yaml 配置到结构体。
 	var cfg Config
-	if err = yaml.Unmarshal(data, &cfg); nil != err {
-		return nil, fmt.Errorf("解析配置文件失败：%v", err)
+	if err := c.Scan(&cfg); err != nil {
+		return nil, err
 	}
 
 	return &cfg, nil
